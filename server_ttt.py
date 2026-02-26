4a814d7a04efccf74874ccc6237f4708655e9d85
dGallier <adrian.gallus@gmx.net>
2026-02-26 11:39:42 +0100

    Add files via upload

---
+++ server_ttt.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, request
+import sqlite3
+
+app = Flask(__name__)
+
+con = sqlite3.connect("Passwörter.db", check_same_thread = False)
+cur = con.cursor()
+
+@app.route("/")
+def start():
+     return render_template("start.html")
+
+@app.route("/registrierung", methods = ["GET", "POST"])
+def registrierung():
+
+    if request.method == "POST":
+        email = request.form["EMail"]
+
+        cur.execute('''
+            SELECT EXISTS(
+            SELECT 1 FROM Passwörter
+            WHERE EMail = ?)
+            ''', [email,])
+        exists = cur.fetchone()[0]
+
+        if exists == 1:
+            print("Ihre E-Mail existiert bereits")
+            return render_template("login.html", exists = True)
+        else:
+            username = request.form["username"]
+            passwort = request.form["password"]
+            
+            cur.execute('''
+                INSERT INTO Passwörter (username, passwort, EMail)
+                VALUES (?, ?, ?)
+                ''', [username, passwort, email])
+            con.commit()
+            return f"Hallo {username}"
+    return render_template("registrierung.html")
+
+@app.route("/login", methods = ['GET', 'POST'])
+def login():  
+    if request.method == 'POST': 
+        
+
+    return render_template("login.html")
+
+@app.route("/main", methods = ['GET', 'POST'])
+def main(): 
+    return
+
+if __name__ == "__main__":
+    app.run(debug = True)
